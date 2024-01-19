1dc63643f629c730388e2c3bc0645dbe1b31fa7f
Daennae <daennae65@gmail.com>
2024-01-20 01:14:49 +0300

    Kivy - front to app

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import custom_tkinter
+import psycopg2
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        lable = Label(text='Программа не работает\nВыключи её')
+
+        return lable
+
+
+if __name__ == "__main__":
+    MyApp.run
